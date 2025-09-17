75bff0589ce00684c85c256d1df79a9a795c1f8a
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-09-17 13:58:28 +0530

    Add files via upload
    This repository provides a practical demonstration of TestNG annotations in Java, including @BeforeSuite, @AfterSuite, @BeforeTest, @AfterTest, @BeforeClass, @AfterClass, @BeforeMethod, and @AfterMethod.
    It contains two test classes with multiple test cases and a testng.xml file to run the suite.
    By executing the suite, you can clearly observe the execution order and lifecycle of TestNG methods.
    This project is intended as a beginner-friendly reference for learning automation test structures with TestNG.

---
+++ Main (5).java
@@ -0,0 +1,360 @@
+package TestNg;
+
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+	
+	@BeforeSuite
+	public void beforeSuit() {
+		System.out.println("Before Suit....");
+	}
+	@AfterSuite
+	public void AfterSuit() {
+		System.out.println("After suit....");
+	}
+	
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("before test.....");
+	}
+	@AfterTest
+	public void AfterTest() {
+		System.out.println("After test....");
+	}
+	
+	@BeforeClass
+	public void beforeclass() {
+		System.out.println("Before class");
+	}
+	@org.junit.AfterClass
+	public void AfterClass() {
+		System.out.println("After class");
+	}
+	
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("Before Method");
+	}
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("After Method ");
+	}
+	
+	@Test
+	public void test1() {
+		System.out.println("This is Test case 1.....");
+	}
+	@Test
+	public void test2() {
+		System.out.println("This is test case 2..... ");
+	}
+
+}
+
+package TestNg;
+
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+	
+	@BeforeSuite
+	public void beforeSuit() {
+		System.out.println("Before Suit....");
+	}
+	@AfterSuite
+	public void AfterSuit() {
+		System.out.println("After suit....");
+	}
+	
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("before test.....");
+	}
+	@AfterTest
+	public void AfterTest() {
+		System.out.println("After test....");
+	}
+	
+	@BeforeClass
+	public void beforeclass() {
+		System.out.println("Before class");
+	}
+	@org.junit.AfterClass
+	public void AfterClass() {
+		System.out.println("After class");
+	}
+	
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("Before Method");
+	}
+	@AfterMethodpackage TestNg;
+
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+	
+	@BeforeSuite
+	public void beforeSuit() {
+		System.out.println("Before Suit....");
+	}
+	@AfterSuite
+	public void AfterSuit() {
+		System.out.println("After suit....");
+	}
+	
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("before test.....");
+	}
+	@AfterTest
+	public void AfterTest() {
+		System.out.println("After test....");
+	}
+	
+	@BeforeClass
+	public void beforeclass() {
+		System.out.println("Before class");
+	}
+	@org.junit.AfterClass
+	public void AfterClass() {
+		System.out.println("After class");
+	}
+	
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("Before Method");
+	}
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("After Method ");
+	}
+	
+	@Test
+	public void test1() {
+		System.out.println("This is Test case 1.....");
+	}
+	@Test
+	public void test2() {
+		System.out.println("This is test case 2..... ");
+	}
+
+}
+
+vpackage TestNg;
+
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+	
+	@BeforeSuite
+	public void beforeSuit() {
+		System.out.println("Before Suit....");
+	}
+	@AfterSuite
+	public void AfterSuit() {
+		System.out.println("After suit....");
+	}
+	
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("before test.....");
+	}
+	@AfterTest
+	public void AfterTest() {
+		System.out.println("After test....");
+	}
+	
+	@BeforeClass
+	public void beforeclass() {
+		System.out.println("Before class");
+	}
+	@org.junit.AfterClass
+	public void AfterClass() {
+		System.out.println("After class");
+	}
+	
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("Before Method");
+	}
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("After Method ");
+	}
+	
+	@Test
+	public void test1() {
+		System.out.println("This is Test case 1.....");
+	}
+	@Test
+	public void test2() {
+		System.out.println("This is test case 2..... ");
+	}
+
+}
+
+package TestNg;
+
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+	
+	@BeforeSuite
+	public void beforeSuit() {
+		System.out.println("Before Suit....");
+	}
+	@AfterSuite
+	public void AfterSuit() {
+		System.out.println("After suit....");
+	}
+	
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("before test.....");
+	}
+	@AfterTest
+	public void AfterTest() {
+		System.out.println("After test....");
+	}
+	
+	@BeforeClass
+	public void beforeclass() {
+		System.out.println("Before class");
+	}
+	@org.junit.AfterClass
+	public void AfterClass() {
+		System.out.println("After class");
+	}
+	
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("Before Method");
+	}
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("After Method ");
+	}
+	
+	@Test
+	public void test1() {
+		System.out.println("This is Test case 1.....");
+	}
+	@Test
+	public void test2() {
+		System.out.println("This is test case 2..... ");
+	}
+
+}
+
+package TestNg;
+
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+	
+	@BeforeSuite
+	public void beforeSuit() {
+		System.out.println("Before Suit....");
+	}
+	@AfterSuite
+	public void AfterSuit() {
+		System.out.println("After suit....");
+	}
+	
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("before test.....");
+	}
+	@AfterTest
+	public void AfterTest() {
+		System.out.println("After test....");
+	}
+	
+	@BeforeClass
+	public void beforeclass() {
+		System.out.println("Before class");
+	}
+	@org.junit.AfterClass
+	public void AfterClass() {
+		System.out.println("After class");
+	}
+	
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("Before Method");
+	}
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("After Method ");
+	}
+	
+	@Test
+	public void test1() {
+		System.out.println("This is Test case 1.....");
+	}
+	@Test
+	public void test2() {
+		System.out.println("This is test case 2..... ");
+	}
+
+}
+
+
+	public void afterMethod() {
+		System.out.println("After Method ");
+	}
+	
+	@Test
+	public void test1() {
+		System.out.println("This is Test case 1.....");
+	}
+	@Test
+	public void test2() {
+		System.out.println("This is test case 2..... ");
+	}
+
+}
+
